a7a26a4243fa7c593a49cdb849b66c236dc5148b
ccliment <ccliment@nigul.cide.es>
2019-11-08 19:18:42 +0100

    Hemos llamado a patata en Main para decir Hola

--- src/proyecto/main.java
+++ src/proyecto/main.java
@@ -1,11 +1,10 @@
 package proyecto;
 
 public class main {
+	
 	public static void main(String[] args) {
-		printHola();
-	}
-	public static void printHola(){
-		System.out.println("Hola món!");
+		patata.printHola();
+		
 	}
 }
 
---
+++ src/proyecto/patata.java
@@ -0,0 +1,11 @@
+package proyecto;
+
+public class patata {
+
+	public static void printHola(){
+		System.out.println("Hola món!");
+		
+		
+	}
+
+}
